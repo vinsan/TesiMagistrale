adc810af14fa133f453125b6c7353fa756252455
Vincenzo <v.santoro21@studenti.unisa.it>
2020-10-22 10:56:14 +0200

    Primo commit

---
+++ TesiIntercettazioniTelefoniche/app/src/main/java/corso/java/tesiintercettazionitelefoniche/MainActivity.java
@@ -0,0 +1,35 @@
+package corso.java.tesiintercettazionitelefoniche;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        TelephonyManager tMgr = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String IMEI = tMgr.getDeviceId();
+        String phoneNumber = tMgr.getLine1Number();
+        Log.d("Intercettazione", "IMEI: "+IMEI+", Numero di telefono: "+phoneNumber);
+    }
+}
